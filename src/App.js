1f4efcd5145ef18ab60a8689aee45d52aca216a6
mikko kotamies <mikko.kotamies@avarko.com>
2018-01-13 16:26:54 +0200

    First version of result page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
+import { Panel } from "react-bootstrap";
+
 import "./App.css";
+import ResultComponent from "./ResultComponent";
+
+const api = "http://rlbackend.azurewebsites.net";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      results: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(api + "/games")
+      .then(response => response.json())
+      .then(res => this.setState({ results: res }))
+      .catch(err => console.log(err));
+  }
+
+  renderResults() {
+    return this.state.results.map((result, index) => (
+      <ResultComponent key={index} {...result} />
+    ));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <div className="layer" />
           <h1 className="App-title">Roguelike hackathon tulokset</h1>
         </header>
+        <div className="Container">{this.renderResults()}</div>
         <p className="App-intro" />
       </div>
     );
---
+++ src/ResultComponent.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Panel } from "react-bootstrap";
+
+import "./App.css";
+
+const ResultComponent = result => (
+  <Panel bsStyle="primary">
+    <Panel.Heading>
+      <Panel.Title componentClass="h3">{result.client}</Panel.Title>
+    </Panel.Heading>
+    <Panel.Body>Kultaa {result.gold || "-"}</Panel.Body>
+    <Panel.Body>XP {result.xp || "-"}</Panel.Body>
+    <Panel.Body>
+      Pelin kesto{" "}
+      {result.durationMinutes
+        ? result.durationMinutes.toFixed(1)
+        : "Kestoa ei saatavilla"}
+    </Panel.Body>
+  </Panel>
+);
+
+export default ResultComponent;
